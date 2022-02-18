89f6722c1a7e22cae5b594c991a886520f1c883a
kannan-santharam <kannan.santharam@freshworks.com>
2022-02-18 19:14:52 +0530

    Create binary-search.js

---
+++ binary-search.js
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/binary-search/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    return nums.findIndex(t => t === target)
+};
